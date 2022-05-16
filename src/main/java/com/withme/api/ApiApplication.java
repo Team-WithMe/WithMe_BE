5dbbbdbdb212f8fc75f639c5dded324c2cb8c834
shshhan <seunghyeonhan95@gmail.com>
2022-05-16 22:37:53 +0900

    DB 연결

--- src/main/java/com/withme/api/ApiApplication.java
+++ src/main/java/com/withme/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.withme.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class})
 public class ApiApplication {
 
 	public static void main(String[] args) {
