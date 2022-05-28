0265b134629253d741b85a438b712c93cdb17bc7
shshhan <seunghyeonhan95@gmail.com>
2022-05-29 00:52:41 +0700

    test url 변경, build시 jar Task skip

--- src/main/java/com/withme/api/api/temp/controller/TempController.java
+++ src/main/java/com/withme/api/api/temp/controller/TempController.java
@@ -4,6 +4,7 @@ import com.withme.api.api.temp.service.TempService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -11,11 +12,12 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/temp")
 public class TempController {
 
     private final TempService service;
 
-    @GetMapping("/temp")
+    @GetMapping("/test")
     public Map<String, String> test(){
         log.trace("trace");
         log.debug("debug");
