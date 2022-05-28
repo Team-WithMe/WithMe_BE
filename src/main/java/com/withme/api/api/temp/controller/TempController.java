d915d0f4f5242f3f34cd0c9e814000dd61a004d6
shshhan <seunghyeonhan95@gmail.com>
2022-05-28 23:53:34 +0700

    logback 설정 및 TempController에 로깅 추가

--- src/main/java/com/withme/api/api/temp/controller/TempController.java
+++ src/main/java/com/withme/api/api/temp/controller/TempController.java
@@ -2,11 +2,13 @@ package com.withme.api.api.temp.controller;
 
 import com.withme.api.api.temp.service.TempService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class TempController {
@@ -15,6 +17,13 @@ public class TempController {
 
     @GetMapping("/temp")
     public Map<String, String> test(){
+        log.trace("trace");
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+
         return service.test();
     }
+
 }
