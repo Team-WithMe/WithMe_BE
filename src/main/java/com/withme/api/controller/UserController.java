a7b31f5edcbd74dafc41cc01d37dd9309d36f3a1
shshhan <seunghyeonhan95@gmail.com>
2022-06-11 22:55:18 +0900

    docs: 회원가입 Swagger

--- src/main/java/com/withme/api/controller/UserController.java
+++ src/main/java/com/withme/api/controller/UserController.java
@@ -2,9 +2,9 @@ package com.withme.api.controller;
 
 import com.withme.api.controller.dto.JoinRequestDto;
 import com.withme.api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
+    @Operation(
+        summary = "회원가입"
+        , description = "새로운 유저의 정보를 DB에 저장한다."
+    )
     @PostMapping("/join")
     public String join(@RequestBody JoinRequestDto dto) {
         log.info("join{} invoked", dto);
         userService.join(dto);
+
+        // Todo 회원 가입 후 return 어떻게 처리할지 프론트와 협의하기
 
         return "join";
     }
--- src/main/java/com/withme/api/controller/dto/JoinRequestDto.java
+++ src/main/java/com/withme/api/controller/dto/JoinRequestDto.java
@@ -2,6 +2,7 @@ package com.withme.api.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.withme.api.domain.user.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,27 +12,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Schema(description = "회원가입을 위한 DTO 객체")
 @Getter
 @NoArgsConstructor
 public class JoinRequestDto {
 
+    @Schema(description = "사용자 이름", example = "withMe", required = true)
     @NotNull
     @Size(min = 3, max = 100)
     private String username;
 
+    @Schema(description = "이메일", example = "withMe@withme.com", required = true)
     @NotNull
     @Size(min = 3, max = 100)
     private String email;
 
+    @Schema(description = "비밀번호", example = "12345", required = true)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull
     @Size(min = 3, max = 100)
     private String password;
 
+    @Schema(description = "닉네임", example = "vV짱짱위드미짱짱Vv", required = true)
     @NotNull
     @Size(min = 3, max = 100)
     private String nickname;
 
+    @Schema(hidden = true)
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Builder
