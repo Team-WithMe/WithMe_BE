bfb26fa5460a68b880dd71ae2d9480befc4dada3
shshhan <seunghyeonhan95@gmail.com>
2022-06-20 20:55:57 +0900

    fix: CORS Preflight 해결 시도_5

--- src/main/java/com/withme/api/config/CorsConfig.java
+++ src/main/java/com/withme/api/config/CorsConfig.java
@@ -15,11 +15,11 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");   //허용할 ip
+        config.addAllowedOrigin("http://localhost:3000");   //허용할 ip
         config.addAllowedHeader("*");   //허용할 header
         config.addAllowedMethod("*");   //허용할 httpMehod(GET, POST, etc..)
 
-        source.registerCorsConfiguration("/*", config);
+        source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
     }
